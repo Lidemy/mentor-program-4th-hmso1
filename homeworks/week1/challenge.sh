38f39b77d7433567cfa5fa52bef1bcd7c15fea75
Iris So <hoiming123456@hotmail.com>
2020-06-18 21:28:00 +0800

    challenge1 finish

---
+++ homeworks/week1/challenge.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+current_number=1
+target=$(($1 + 1))
+
+
+while [ $current_number -ne $target ]
+do
+    
+    touch "${current_number}.js";
+    echo "檔案建立完成";
+    current_number=$((current_number + 1))
+done

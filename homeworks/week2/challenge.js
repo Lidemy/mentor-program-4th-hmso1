b5ab8e1c6ae20ecf89e718acaf3d1d002bf4ac74
Iris So <iris_so@sasa.com>
2020-06-24 16:34:34 +0800

    complete challenge

--- homeworks/week2/challenge.js
+++ homeworks/week2/challenge.js
@@ -1,123 +1,29 @@
 /*
-1. 找出陣列的中位數，檢查是否等於 n
-2. 是，回傳 n 的位置
-3. 
-4. 判斷 n 是否 > 中位數 && 右陣列數值是否不等於空陣列
-5. 是，將右陣列和目標數值傳到 step 1
-6. 否，判斷 n 是否 < 中位數 && 左陣列數值是否不等於空陣列
-7. 是，將左陣列和目標數值傳到 step 1
-8. 否，return -1
-*/
-
-console.time('process');
+1. 設定陣開始變數等 0，結束變數等於陣列總數 - 1
+2. while (當結束位置 - 開始位置 > 0)
+3. 找出陣列的中位數位置，檢查該位置元素是否等於 n 
+4. 是，return 中位數的位置
+5. 不是，判斷 n 是否大過中位數
+6. 是，陣列的開始變為中位數位置 +1，結束不變
+7. 回到 step 2
+8. 不，陣列的開始不變，結束變為中位數位置 -1
+9. 回到 step 2
+10. 完成 while loop, return -1
+*/ 
 
 function search(arr, n) {
-    for(var i=0; i<arr.length; i++) {
-      if (arr[i] === n) return i
+  var start = 0
+  var end = arr.length - 1
+
+  while((end - start) > 0 ){
+    var middle = Math.ceil(end - start)
+
+    if (arr[middle] === n){
+      return middle
     }
-    return -1
+
+    (n > arr[middle]) ? start = middle + 1 :  end = middle - 1
+  }
+  return -1
 }
 
-// var location = 0
-// var notFound = false
-// function splitArray(oldArr, k){
-//   var arr1 = oldArr.slice(0, k)
-//   var arr2 = oldArr.slice(k+1)
-//   return {'leftArr': arr1,  'rightArr': arr2}
-// }
-
-// function search(arr, n){
-//   var arrMiddle = Math.floor(arr.length / 2)
-  
-//   if (arr[arrMiddle] === n) {
-//     return arrMiddle
-//   }
-
-//   newArr = splitArray(arr, arrMiddle)
-//   if (n > arr[arrMiddle] && newArr.rightArr !== []) {
-//     location = arrMiddle + 1 + search(newArr.rightArr, n)
-//   } else if (n < arr[arrMiddle] && newArr.leftArr !== []) {
-//     location = search(newArr.leftArr, n)
-//   } else {
-//     notFound = true
-//   }
-
-//   return(notFound === true ? -1 : location)
-// }
-
-console.log(search([1,2,3,4,5],6))
-// notFound = false
-console.log(search([1,2,3,4,5],5))
-// notFound = false
-console.log(search([1,2,3,4,5],1))
-console.timeEnd('process');
-
-
-
-// var readline = require('readline');
-
-// var lines = []
-// var rl = readline.createInterface({
-//   input: process.stdin
-// });
-
-// rl.on('line', function (line) {
-//   lines.push(line)
-// });
-
-// rl.on('close', function() {
-//   solve(lines)
-// })
-
-// function search(arr, n) {
-//     for(var i=0; i<arr.length; i++) {
-//       if (arr[i] === n) return i
-//     }
-//     return -1
-// }
-
-// var location = 0
-// var notFound = false
-// function splitArray(oldArr, k){
-//   var arr1 = oldArr.slice(0, k)
-//   var arr2 = oldArr.slice(k+1)
-//   return {'leftArr': arr1,  'rightArr': arr2}
-// }
-
-// function search(arr, n){
-//   var arrMiddle = Math.floor(arr.length / 2)
-  
-//   if (arr[arrMiddle] === n) {
-//     return arrMiddle
-//   }
-
-//   newArr = splitArray(arr, arrMiddle)
-//   if (n > arr[arrMiddle] && newArr.rightArr !== []) {
-//     location = arrMiddle + 1 + search(newArr.rightArr, n)
-//   } else if (n < arr[arrMiddle] && newArr.leftArr !== []) {
-//     location = search(newArr.leftArr, n)
-//   } else {
-//     notFound = true
-//   }
-
-//   return(notFound === true ? -1 : location)
-// }
-
-// function solve(lines) {
-//     var temp = lines[0].split(' ')
-//     var originArr = []
-//     var target = []
-
-//     for(i=1; i<=temp[0]; i++){
-//       originArr.push(Number(lines[i]))
-//     }
-    
-//     for(i=Number(temp[0])+1; i<lines.length; i++){
-//       target.push(Number(lines[i]))
-//     }
-
-//     for(var i=0; i<target.length; i++){
-//       notFound = false
-//       console.log(search(originArr, target[i]))
-//     }
-// }

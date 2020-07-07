84d73f48d91911b481d5cf80b636cf6ca115c101
Iris So <hoiming123456@hotmail.com>
2020-07-08 00:20:21 +0800

    working on hw2 完成 list, read, delete, create

--- homeworks/week4/hw2.js
+++ homeworks/week4/hw2.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-plusplus */
+/* eslint-disable no-plusplus, quotes, quote-props */
 const request = require('request');
 const process = require('process');
 
@@ -9,7 +9,7 @@ function get20BookList() {
     'https://lidemy-book-store.herokuapp.com/books?_limit=20',
     (error, response, body) => {
       const json = JSON.parse(body);
-      for (let i = 0; i < 20; i++) {
+      for (let i = 0; i < json.length; i++) {
         console.log(`${json[i].id} ${json[i].name}`);
       }
     },
@@ -18,12 +18,35 @@ function get20BookList() {
 
 function getBookId(n) {
   request(
-    `https://lidemy-book-store.herokuapp.com/books/ ${n}`,
+    `https://lidemy-book-store.herokuapp.com/books/${n}`,
     (error, response, body) => {
       const json = JSON.parse(body);
-      console.log(json.name);
+      if (json.name === undefined) {
+        console.log('No this book in the API, please try another ID');
+      } else {
+        console.log(json.name);
+      }
     },
   );
+}
+
+function createBook(n) {
+  request(
+    'https://lidemy-book-store.herokuapp.com/books/',
+    (error, response, body) => {
+      const num = JSON.parse(body).length;
+
+      request.post({
+        url: 'https://lidemy-book-store.herokuapp.com/books/',
+        json: true,
+        body: { "id": num, "name": n },
+      });
+    },
+  );
+}
+
+function deleteBookId(n) {
+  request.del(`https://lidemy-book-store.herokuapp.com/books/${n}`);
 }
 
 switch (action) {
@@ -34,10 +57,10 @@ switch (action) {
     getBookId(process.argv[3]);
     break;
   case 'delete':
-    // deleteBookId(process.argv[3])
+    deleteBookId(process.argv[3]);
     break;
   case 'create':
-    // createBook(process.argv[3])
+    createBook(process.argv[3]);
     break;
   case 'update':
     // patchBook(process.argv[3], process.argv[4])

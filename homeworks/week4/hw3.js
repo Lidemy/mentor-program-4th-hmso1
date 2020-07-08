58b0b6cd0594a94367ae7b33a33a7849caca49a3
Iris So <iris_so@sasa.com>
2020-07-08 17:19:38 +0800

    complete hw3

---
+++ homeworks/week4/hw3.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-plusplus, indent, no-unused-expressions */
+const request = require('request');
+const process = require('process');
+
+function printInfo(s) {
+  for (let i = 0; i < s.length; i++) {
+    console.log(
+`============
+國家：${(s[i].name)}
+首都：${(s[i].capital)}
+貨幣：${(s[i].currencies[0].code)}
+國碼：${(s[i].callingCodes[0])}`,
+    );
+  }
+}
+
+request(
+  `https://restcountries.eu/rest/v2/name/${process.argv[2]}`,
+  (error, response, body) => {
+    const json = JSON.parse(body);
+    (json.status === 404) ? console.log('找不到國家資訊') : printInfo(json);
+  },
+);

161a793db246fab5157b9a45b031584cf25df4d0
Iris So <hoiming123456@hotmail.com>
2020-08-27 00:16:23 +0800

    finish hw1

---
+++ homeworks/week9/hw1/handle_add_comment.php
@@ -0,0 +1,25 @@
+<?php
+  session_start();
+  require_once('conn.php');
+  require_once('utils.php');
+  
+  if (empty($_POST['comment'])) {
+    header('Location:index.php?errCode=1');
+    die();
+  }
+
+  $user = getUserFromSession($_SESSION['username']);
+  $nickname = $user['nickname'];
+  $comment = $_POST['comment'];
+
+  $sql = sprintf('INSERT INTO comments(nickname, comment) VALUES("%s", "%s")', $nickname, $comment);
+  $result = $conn->query($sql);
+
+  if (!$result) {
+    die($conn->error);
+  }
+
+  header('Location: index.php');
+
+?>
+
---
+++ homeworks/week9/hw1/handle_register.php
@@ -0,0 +1,29 @@
+<?php
+  require_once('conn.php');
+  
+  if (empty($_POST['username']) ||
+      empty($_POST['nickname']) ||
+      empty($_POST['password'])) {
+
+    header('Location:register.php?errCode=1');
+    die();
+    }
+
+  $username = $_POST['username'];
+  $nickname = $_POST['nickname'];
+  $password = $_POST['password'];
+
+  $sql = sprintf('INSERT INTO users(username, nickname, password) VALUES("%s", "%s", "%s")', $username, $nickname, $password);
+  $result = $conn->query($sql);
+
+  if (!$result) {
+    if ($conn->errno === 1062) {
+      header('Location:register.php?errCode=2');
+    }
+    die($conn->error);
+  }
+
+  header('Location: index.php');
+
+?>
+
